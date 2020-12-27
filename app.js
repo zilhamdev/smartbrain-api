dfe8fc701cd98911013944886dcf7e288934f01c
zilhamdev <zilhamdev@gmail.com>
2020-12-28 00:41:08 +0700

    changed database

--- app.js
+++ app.js
@@ -12,12 +12,7 @@ const image = require("./controllers/image");
 
 const db = knex({
     client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: '123456',
-        database: 'smartbrain'
-    }
+    connection: process.env.DATABASE_URL
 });
 
 app.use(express.urlencoded({ extended: false }));
