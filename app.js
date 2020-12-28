78927bd5613a34b1298988af0fedb1c6a9c41035
zilhamdev <zilhamdev@gmail.com>
2020-12-28 11:49:05 +0700

    change connection 3

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ const image = require("./controllers/image");
 const db = knex({
     client: 'pg',
     connection: {
-        connectString: process.env.DATABASE_URL,
-        ssl: true
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
     }
 });
 
